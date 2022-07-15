ffdb41ba297985e7f302d922b243df5c6d764ea0
Carlos Miranda <carlosmarban@outlook.com>
2022-07-15 00:27:30 -0500

    15 julio

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import marban.library.Console;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Integer x = Console.readInteger("cual es tu edad: ");
+        System.out.println("x = " + x);
     }
 }
--- src/marban/library/Test.java
+++
@@ -1,9 +0,0 @@
-package marban.library;
-public class Test {
-    public static void main(String[] args) {
-        Console.writeln("Hello!!");
-        Integer x = Console.readInteger("cual es tu edad: ");
-        System.out.println("x = " + x);
-
-    }
-}
