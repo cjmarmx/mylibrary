3bd252f8a462974d6a6e9ba1a5125878448114aa
Carlos Miranda <carlosmarban@outlook.com>
2022-07-14 23:56:09 -0500

    version july 14

--- src/marban/library/Console.java
+++ src/marban/library/Console.java
@@ -1,36 +1,40 @@
 package marban.library;
-
-import java.util.InputMismatchException;
 import java.util.Scanner;
-
 /**
  * @author Carlos Miranda
  */
 public class Console {
     private static Scanner input = new Scanner(System.in);
-
+    // cancels the create Console type objects
     private Console() {
-    } // cancels the create Console type objects
-
+    }
     /**
-     * Print an Object to the Console
+     * write() Print an Object to the Console
+     *
      * @param x the Object to print
      */
-    public static void write(Object x) { System.out.print(x); }
+    public static void write(Object x) {
+        System.out.print(x);
+    }
     /**
-     * Print an Object to the Console and skip the line
+     * writeln() Print an Object to the Console and skip the line
+     *
      * @param x the Object to print
      */
-    public static void writeln(Object x) { System.out.println(x); }
-
+    public static void writeln(Object x) {
+        System.out.println(x);
+    }
     /**
-     * Return an String Object catched in the Console by the user
+     * read() Return an String Object catched in the Console by the user
+     *
      * @return an String Object
      */
-    public static String read() { return input.nextLine(); }
-
+    public static String read() {
+        return input.nextLine();
+    }
     /**
-     * Prints the String Object as Console Message and after gets the String enter by the User
+     * read(String message) Prints the String Object as Console Message and after gets the String enter by the User
+     *
      * @param message to print
      * @return an String Object
      */
@@ -38,19 +42,87 @@ public class Console {
         Console.writeln(message);
         return input.nextLine();
     }
+    /**
+     * readInteger() Get an Integer and return it
+     * @return an Integer Object
+     */
+    public static Integer readInteger() {
+        try {
+            return Integer.parseInt(Console.read());
+        } catch (NumberFormatException e) {
+            System.out.println("Must enter an Integer, try again..");
+            readInteger();
+        }
+        return 404;
+    }
+    /**
+     * readInteger(String message) Show a console message, get an Integer and return it
+     * @param message to print
+     * @return an Integer Object
+     */
+    public static Integer readInteger(String message) {
+        try {
+            return Integer.parseInt(Console.read(message));
+        } catch (NumberFormatException e) {
+            System.out.println("Must enter an Integer, try again..");
+            readInteger(message);
+        }
+        return Integer.parseInt(Console.read(message));
+    }
 
-    public static Integer readInt() {
-        Integer integerNumber = 0;
-        boolean continua;
-        do {
-            try {
-                integerNumber = input.nextInt();
-                //return x;
-            } catch (InputMismatchException e) {
-                Console.writeln("Must enter an Integer among 1 and 2,147,483,647:");
-                Console.readInt();
-                continua = true;
-            }
-        } while (continua);
-    } return integerNumber;
+    /**
+     * readFloat() Get a Float and return it
+     * @return a Float Object
+     */
+    public static Float readFloat() {
+        try {
+            return Float.parseFloat(Console.read());
+        } catch (NumberFormatException e) {
+            System.out.println("Must enter a Float, try again..");
+            readFloat();
+        }
+        return 404.404f;
+    }
+    /**
+     * readFloat(String message) Show message, get a Float and return it
+     * @param message to print
+     * @return a Float Object
+     */
+    public static Float readFloat(String message) {
+        try {
+            return Float.parseFloat(Console.read(message));
+        } catch (NumberFormatException e) {
+            System.out.println("Must enter a Float, try again..");
+            readFloat(message);
+        }
+        return 404.404f;
+    }
+    /**
+     * readDouble() get a Double and return it
+     *
+     * @return a Double Object
+     */
+    public static Double readDouble() {
+        try {
+            return Double.parseDouble(Console.read());
+        } catch (NumberFormatException e) {
+            System.out.println("Must enter a Double, try again..");
+            readDouble();
+        }
+        return 404.404d;
+    }
+    /**
+     * readDouble(String message) Show message, get a Double and return it
+     * @param message to print
+     * @return a Double Object
+     */
+    public static Double readDouble(String message) {
+        try {
+            return Double.parseDouble(Console.read(message));
+        } catch (NumberFormatException e) {
+            System.out.println("Must enter a Double, try again..");
+            readDouble(message);
+        }
+        return 404.404;
+    }
 }
--- src/marban/library/Test.java
+++ src/marban/library/Test.java
@@ -1,11 +1,9 @@
-package marban.librerias;
-
+package marban.library;
 public class Test {
     public static void main(String[] args) {
-        Console.writeln("What is your name?:");
-        String name = Console.read("escribe tu nombre:");
-        Console.writeln("Escribe tu edad:");
-        Integer age = Console.readInt();
-        Console.write("Hi " + name + "! you are " + age + " years old");
+        Console.writeln("Hello!!");
+        Integer x = Console.readInteger("cual es tu edad: ");
+        System.out.println("x = " + x);
+
     }
 }
